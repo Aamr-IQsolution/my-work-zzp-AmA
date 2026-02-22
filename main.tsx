9c667bb7afd4d9400addaa0e93c07889b2a200e3
al.admeral.ama <al.admeral.ama@gmail.com>
2026-02-22 13:11:25 +0000

    Make web site ad PWA

--- main.tsx
+++ main.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/react";
+import { registerSW } from 'virtual:pwa-register';
+
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
